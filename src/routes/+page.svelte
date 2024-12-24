150193737aaa2897de18e5b21f208ce97d9bee64
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-24 16:03:38 +0000

    bigger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,10 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        height: 100vh;
     }
     p {
         font-family: "Arial, Helvetica, sans-serif";
+        font-size: 7em;
     }
 </style>
