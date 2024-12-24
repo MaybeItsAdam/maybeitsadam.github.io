a9c1d40cf224e9698664f2462b03bb869faa9fbb
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-24 14:27:36 +0000

    v1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+    let { children } = $props();
+</script>
+
+<nav>
+    <a href="{base}/">home</a>
+    <a href="{base}/projects">projects</a>
+</nav>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<h1>YAYY Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<h1>home</h1>
+<p>there will be stuff here</p>
