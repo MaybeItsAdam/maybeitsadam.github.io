dbb01cce004b6349cca6abcc869cafef9237aa21
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-26 13:46:57 +0000

    Updates

--- _app/immutable/entry/start.CIqE51dT.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DdaGypbd.js";export{t as start};
---
+++ _app/immutable/nodes/3.ovjiyJq0.js
@@ -0,0 +1,1 @@
+import{a,t as e}from"../chunks/disclose-version.CIozeill.js";import"../chunks/legacy.DsPB_jT1.js";import{n as p}from"../chunks/runtime.DrY0v0QI.js";var m=e("<h1>projects</h1> <p>sometimes i make stuff</p>",1);function f(t){var o=m();p(2),a(t,o)}export{f as component};
