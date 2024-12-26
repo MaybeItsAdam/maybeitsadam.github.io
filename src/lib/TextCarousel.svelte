7b45a14337f48f759148f8bcdf3b0a64eed49cd2
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-26 17:23:42 +0000

    manual shift

--- src/lib/TextCarousel.svelte
+++ src/lib/TextCarousel.svelte
@@ -5,9 +5,7 @@
     let nameList: string[];
     export let finalName: string = "";
 
-    let currentIndex = 0;
-    const transitionSpeed = 500; // Time for the transition of each name in milliseconds
-    const delay = 2000; // Time between name transitions
+    let ismoving = false;
 
     const generateNames = async (numOfNames: number): Promise<string[]> => {
         const response = await fetch("/names.txt");
@@ -23,20 +21,42 @@
         nameList = await generateNames(3);
         nameList.push(finalName);
     });
+
+    let currentShift = 0; // Tracks the current shift count
+    let shiftVal = 0; // Tracks the current top offset
+    let shiftInterval: number;
+
+    function startShifting() {
+        // Start shifting the div every x milliseconds
+        shiftInterval = setInterval(() => {
+            if (currentShift < nameList.length) {
+                currentShift++;
+                shiftVal -= currentShift;
+            } else {
+                clearInterval(shiftInterval);
+            }
+        }, 800); // Interval time (in milliseconds, e.g., 300ms)
+    }
 </script>
 
 <div id="cwrapper" style="--chwidth: {finalName.length}ch">
     {#each nameList as name}
-        <div>{name}</div>
+        <div class="mover" style="--shift: {shiftVal}ch">{name}</div>
     {/each}
 </div>
 
 <style>
     #cwrapper {
         display: inline-flex;
+        position: relative;
         flex-direction: column;
         align-content: flex-start;
         width: var(--chwidth);
         height: 1ch;
+    }
+    .mover {
+        position: relative;
+        transition: up 0.3s ease;
+        up: var(--shiftVal);
     }
 </style>
