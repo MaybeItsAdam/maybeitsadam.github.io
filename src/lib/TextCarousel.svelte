88828029498c68bfd00fa0e0e727c2b0521a2888
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-26 21:26:54 +0000

    left

--- src/lib/TextCarousel.svelte
+++ src/lib/TextCarousel.svelte
@@ -80,7 +80,7 @@
     }
 
     #mask {
-        display: inline-block;
+        display: inline;
         right: -100%;
         bottom: -2em;
         mask-image: linear-gradient(
@@ -98,5 +98,6 @@
             transparent
         );
         pointer-events: none;
+        align-content: start;
     }
 </style>
