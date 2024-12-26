f9ee4d4dffdb92128ccfe6799ba73ad6594a743e
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2024-12-26 23:24:38 +0000

    black

--- src/lib/TextCarousel.svelte
+++ src/lib/TextCarousel.svelte
@@ -81,9 +81,8 @@
     #cwrapper::after {
         content: "";
         position: absolute;
-        bottom: 0;
         left: 0;
-        right: 0;
+        width: 100%;
         height: 2em; /* Adjust to the height you want for the masking */
         background: linear-gradient(
             to bottom,
